9475bcf3434b6176668a81770d758da654df5092
orangeoctopus <chocolate.phireworkz+github@gmail.com>
2016-11-09 00:16:14 +1100

    almost done except split string always 0

---
+++ angmod1-soln/js/app.js
@@ -0,0 +1,39 @@
+(function() {
+'use strict';
+
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.checkTouch = function () {
+	if($scope.foodItems.length == 0) {
+		$scope.checkMessage = "Please enter data first";
+	   } else {
+		var numberOfItems = getCount($scope.foodItems);
+	   $scope.checkMessage = getMessage(numberOfItems);
+	}
+};
+
+	
+	
+}
+
+function getCount(foodList) {
+	
+	var splitList = foodList.split(foodList, ',');
+	console.log(foodList);
+	console.log(splitList);
+	return splitList.length;
+}
+
+function getMessage(number) {
+	if (number <= 3) {
+       return "Enjoy!";
+	} else {
+	   return "Too Much!";
+	}
+}
+
+
+})();
