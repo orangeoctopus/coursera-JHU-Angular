e5c68d17e0366fcfe2dc5dcc46debb059e31056e
orangeoctopus <chocolate.phireworkz+github@gmail.com>
2016-11-21 17:54:53 +1100

    up to directives

--- angmod3-soln/js/app.js
+++ angmod3-soln/js/app.js
@@ -5,9 +5,34 @@ angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService',MenuSearchService)
 .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.directive('foundItems', FoundItems)
+
+function FoundItems() {
+	var ddo = {
+		templateUrl: 'foundItems.html',
+		scope: {
+			foundItems: '<',
+			onRemove: '='
+		}
+	}
+}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
+	var narrowMenu = this;
+
+
+	narrowMenu.searchMenu = function (searchTerm) {
+		// body...
+		console.log("searched term:" + narrowMenu.searchTerm);
+		
+		narrowMenu.items = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+		
+		
+		
+	};
+
 
 }
 
@@ -21,17 +46,42 @@ function MenuSearchService($http, ApiBasePath) {
 	//gets all the menu items, it should loop through them to pick out the 
 	//ones whose description matches the searchTerm. Once a list of found items is 
 	//compiled, it should return that list (wrapped in a promise).
-
 	service.getMatchedMenuItems = function (searchTerm) {
+		var promise = getMenuItems();
+		var foundItems = [];
+		promise.then(function (response) {
+     	 console.log(response.data);
+      	 
+		 for (var i = 0; i < response.data.menu_items.length; i++) {
+    			var menuItem = response.data.menu_items[i];
 
-		var response = $http({
+    			if(menuItem.description.toLowerCase().indexOf(searchTerm.toLowerCase()) != -1) {
+    				foundItems.push(menuItem);
+    				console.log(menuItem);
+    			}
+    		}
+    		    // return processed items
+    		return foundItems;
+
+
+    	})
+    	.catch(function (error) {
+    	  console.log(error);
+    	})
+
+		return "sdfd";
+
+		
+};
+
+function getMenuItems() {
+    var response = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
-     });
+    });
 
     return response;
-
-	};
+  };
 }
 
 })();
